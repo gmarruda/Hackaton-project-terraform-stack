d17211a1a379ebbdf2f27f9b0658d0f7e879dfb3
Ubuntu <ubuntu@ip-172-31-24-221.ec2.internal>
2021-03-08 23:17:41 +0000

    exercicio 4

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-22cld-338614"
+    bucket = "hackaton-fiap-22cld-cgip"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
